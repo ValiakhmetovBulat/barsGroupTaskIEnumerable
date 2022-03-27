0cf49934ad1a8dba8c7c8db26830d4bc69a1d629
ValiakhmetovBulat <valiaxmetovb134@gmail.com>
2022-03-27 23:46:47 +0300

    task 1 finished

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,9 +1,18 @@
 ﻿using barsGroupTaskIEnumerable;
 
 string filePath = @"..\..\..\text.txt";
+// string
 var generic = new LocalFileLogger<string>(filePath);
 
 generic.LogInfo("Hello!");
 Exception ex = new DivideByZeroException();
 generic.LogError("Division", ex);
 generic.LogWarning("Warning!");
+
+// integer
+var generic1 = new LocalFileLogger<int>(filePath);
+
+generic.LogInfo("My integer");
+Exception ex1 = new DataMisalignedException();
+generic.LogError("Data misaligned", ex1);
+generic.LogWarning("Waning!");
